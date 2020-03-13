6a0cb902f4131dd6214b23c5507a1beadb7ce424
Gerardo Coss Alcantar <gcoss@cinepolis.com>
2020-03-12 20:10:26 -0600

    [add] download mp3

--- hola.js
+++ hola.js
@@ -1,35 +1,35 @@
 // Scraping
-// var fs = require('fs'),
-//     request = require('request');
+var fs = require('fs'),
+    request = require('request');
 
-// var download = function(uri, filename, callback){
-//   request.head(uri, function(err, res, body){
-//     console.log('content-type:', res.headers['content-type']);
-//     console.log('content-length:', res.headers['content-length']);
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
 
-//     request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-//   });
-// };
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
 
-// download('https://img.pokemondb.net/sprites/sun-moon/icon/bulbasaur.png', 'google.png', function(){
-//   console.log('done');
-// });
+download('http://233a48f2eb5b429bf09a-369bcdcd9a2663001128510c5f8d2279.r41.cf1.rackcdn.com/CG-E759-20200309.mp3', 'dd.mp3', function(){
+  console.log('done');
+});
 
-const cheerio = require('cheerio');
-const axios = require('axios');
+// const cheerio = require('cheerio');
+// const axios = require('axios');
 
-axios.get('https://pokemondb.net/pokedex/all')
-    .then(response =>{
-      const $ = cheerio.load(response.data);
-      const items = $('tr').toArray()
-            .map(item=>{
-              const $item = $(item);
-              return{
-                numero: $item.find('.cell-fixed .infocard-cell-data').text(),
-              }
-            });
-            debugger;
-    });
+// axios.get('https://pokemondb.net/pokedex/all')
+//     .then(response =>{
+//       const $ = cheerio.load(response.data);
+//       const items = $('tr').toArray()
+//             .map(item=>{
+//               const $item = $(item);
+//               return{
+//                 numero: $item.find('.cell-fixed .infocard-cell-data').text(),
+//               }
+//             });
+//             debugger;
+//     });
 
 // inyectar jquery
 // var script = document.createElement('script');
